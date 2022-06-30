c6f5d9be50d516f5c38c85fdbc50acfe68f82e8a
Adrian Stępkowski <adrianstepkowski@gmail.com>
2022-06-30 12:23:23 +0200

    build.sh removed

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# README.md
-# https://github.com/Rahix/avr-hal/issues/124#issuecomment-766841616
-cargo +nightly-2021-01-07 build --target avr-atmega8.json -Z build-std=core --release
